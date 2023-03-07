221b28ca40e47b9e49b6fbee7d02fb8ab639774d
Muganwa <stevenmuganwa@live.com>
2023-03-07 10:08:35 +0300

    upgrades

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-var routes = require('./api/routes/routes');
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./api/routes/routes');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
 routes(app);
 app.listen(port, function () {
     console.log('Server started on port: ' + port);
